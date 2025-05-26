af9f4a4c333c8b723ebda78613ffb2b2d8faf9c6
David <david48kariuki@gmail.com>
2025-05-26 21:10:08 +0300

    replaced vet_vaccination.db

--- database.py
+++ database.py
@@ -5,3 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 engine = create_engine('sqlite:///vet_vaccination.db')
 Session = sessionmaker(bind=engine)
+
+from models.animal import Animal
+from models.owner import Owner
+from models.vaccination import Vaccination
